3b9d9cbe3b0fbdb36c9a32a29cabef1703b38476
shweta.joshi <shweta.joshi@harbingergroup.com>
2019-04-16 18:27:12 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { Link } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <h3>Want to go :</h3><Link to='/login'>LOGIN</Link><hr />
+        <h3>Want to go :</h3><Link to='/register'>REGISTER</Link>
       </div>
     );
   }
